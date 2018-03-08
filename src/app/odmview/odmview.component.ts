81ca17c893cc03586c5486750b903fcbb4b3808e
Masafumi Okada <mf@okadajp.org>
2018-03-08 18:24:05 +0900

    - support Update button and inactivating drop area. - support improved display of ODM and pager.

--- src/app/odmview/odmview.component.ts
+++ src/app/odmview/odmview.component.ts
@@ -9,31 +9,39 @@ import { UploadEvent, UploadFile } from 'ngx-file-drop';
 export class OdmviewComponent implements OnInit {
   public files: UploadFile[] = [];
   public odm: Document;
-  public dispODM : Array<Object> = [ { Name:"Dummy", Value: "Hummy"}];
+  public dispODM : Array<Object> = [ {  FormName: "Form Not uploaded",
+                                        Sections: [ { SectionName: "Undefined",
+                                                      Items:[ { Name: "Undefined",
+                                                                Value: "Undefined"}]
+                                                    }]
+                                      }];
+  public pageStart = 0;
 
   public loadODM(text: string) {
-    var viewODM = [];
+    this.dispODM = [];
     var parser = new DOMParser();
     this.odm = parser.parseFromString(text, 'text/xml');
-    var forms = this.odm.getElementsByTagName('FormDef');
-    var itemgrouprefs = forms[0].getElementsByTagName('ItemGroupRef');
-    var itemgroupdef;
-    for(var i=0;  i < itemgrouprefs.length; i++) {
-      console.log(itemgrouprefs[i]);
-      var itemgroupoid = itemgrouprefs[i].getAttribute('ItemGroupOID');
-      itemgroupdef = this.odm.querySelector('[OID=\''+itemgroupoid+'\']');
-      viewODM.push({ Name: itemgroupdef.getAttribute('Name', ""), Value: "ItemGroup"});
-      var itemrefs = itemgroupdef.getElementsByTagName('ItemRef');
-      for(var j=0 ; j < itemrefs.length; j++) {
-        console.log(itemrefs[j]);
-        var itemdefoid = itemrefs[j].getAttribute('ItemOID');
-        var itemdef = this.odm.querySelector('[OID=\''+itemdefoid+'\']');
-        viewODM.push({ Name: itemdef.getAttribute('Name'), Value: itemdef.getAttribute("DataType")});
+    var formselements = this.odm.getElementsByTagName('FormDef');
+    for(var k=0; k < formselements.length ; k++) {
+      var sections: Array<Object> = [ ];
+      var aform = { FormName: formselements[k].getAttribute('Name'), Sections: sections };
+      var itemgrouprefs = formselements[k].getElementsByTagName('ItemGroupRef');
+      for(var i=0;  i < itemgrouprefs.length; i++) {
+        var itemgroupoid = itemgrouprefs[i].getAttribute('ItemGroupOID');
+        var itemgroupdef = this.odm.querySelector('[OID=\''+itemgroupoid+'\']');
+        var items : Array<Object> = [ ];
+        var asection = { SectionName: itemgroupdef.getAttribute('Name'), Items : items } ;
+        var itemrefs = itemgroupdef.getElementsByTagName('ItemRef');
+        for(var j=0 ; j < itemrefs.length; j++) {
+          var itemdefoid = itemrefs[j].getAttribute('ItemOID');
+          var itemdef = this.odm.querySelector('[OID=\''+itemdefoid+'\']');
+          items.push({ Name: itemdef.getAttribute('Name'), Value: itemdef.getAttribute("DataType")});
+        }
+        sections.push(asection);
       }
+      this.dispODM.push(aform);
     }
-    this.dispODM  = viewODM.slice();
-    console.log(this.dispODM);
-    }
+  }
   public dropped(event: UploadEvent) {
     this.files = event.files;
     for (const file of event.files) {
@@ -41,16 +49,26 @@ export class OdmviewComponent implements OnInit {
         let reader:FileReader = new FileReader();
         var comp = this;
         reader.onloadend =  function(e)  {
-          console.log(this.result);
+          // console.log(this.result);
           comp.loadODM(this.result);
         };
         reader.readAsText(odmfile);
       });
     }
   }
+
   constructor() {
   }
+
   ngOnInit() {
   }
 
+  update() {
+    console.log(this.dispODM);
+    console.log(this.pageStart);
+  }
+
+  pager(page: number) {
+    this.pageStart = page;
+  }
 }

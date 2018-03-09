59bf82227ea31b50024c2e87eb881955ad6e6413
Masafumi Okada <mf@okadajp.org>
2018-03-09 14:01:54 +0900

    Beautify source.

--- src/app/odmview/odmview.component.ts
+++ src/app/odmview/odmview.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { UploadEvent, UploadFile } from 'ngx-file-drop';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  UploadEvent,
+  UploadFile
+} from 'ngx-file-drop';
 
 @Component({
   selector: 'app-odmview',
@@ -9,12 +15,16 @@ import { UploadEvent, UploadFile } from 'ngx-file-drop';
 export class OdmviewComponent implements OnInit {
   public files: UploadFile[] = [];
   public odm: Document;
-  public dispODM : Array<Object> = [ {  FormName: "Form Not uploaded",
-                                        Sections: [ { SectionName: "Undefined",
-                                                      Items:[ { Name: "Undefined",
-                                                                Value: "Undefined"}]
-                                                    }]
-                                      }];
+  public dispODM: Array < Object > = [{
+    FormName: "Form Not uploaded",
+    Sections: [{
+      SectionName: "Undefined",
+      Items: [{
+        Name: "Undefined",
+        Value: "Undefined"
+      }]
+    }]
+  }];
   public pageStart = 0;
 
   public loadODM(text: string) {
@@ -22,20 +32,29 @@ export class OdmviewComponent implements OnInit {
     var parser = new DOMParser();
     this.odm = parser.parseFromString(text, 'text/xml');
     var formselements = this.odm.getElementsByTagName('FormDef');
-    for(var k=0; k < formselements.length ; k++) {
-      var sections: Array<Object> = [ ];
-      var aform = { FormName: formselements[k].getAttribute('Name'), Sections: sections };
+    for (var k = 0; k < formselements.length; k++) {
+      var sections: Array < Object > = [];
+      var aform = {
+        FormName: formselements[k].getAttribute('Name'),
+        Sections: sections
+      };
       var itemgrouprefs = formselements[k].getElementsByTagName('ItemGroupRef');
-      for(var i=0;  i < itemgrouprefs.length; i++) {
+      for (var i = 0; i < itemgrouprefs.length; i++) {
         var itemgroupoid = itemgrouprefs[i].getAttribute('ItemGroupOID');
-        var itemgroupdef = this.odm.querySelector('[OID=\''+itemgroupoid+'\']');
-        var items : Array<Object> = [ ];
-        var asection = { SectionName: itemgroupdef.getAttribute('Name'), Items : items } ;
+        var itemgroupdef = this.odm.querySelector('[OID=\'' + itemgroupoid + '\']');
+        var items: Array < Object > = [];
+        var asection = {
+          SectionName: itemgroupdef.getAttribute('Name'),
+          Items: items
+        };
         var itemrefs = itemgroupdef.getElementsByTagName('ItemRef');
-        for(var j=0 ; j < itemrefs.length; j++) {
+        for (var j = 0; j < itemrefs.length; j++) {
           var itemdefoid = itemrefs[j].getAttribute('ItemOID');
-          var itemdef = this.odm.querySelector('[OID=\''+itemdefoid+'\']');
-          items.push({ Name: itemdef.getAttribute('Name'), Value: itemdef.getAttribute("DataType")});
+          var itemdef = this.odm.querySelector('[OID=\'' + itemdefoid + '\']');
+          items.push({
+            Name: itemdef.getAttribute('Name'),
+            Value: itemdef.getAttribute("DataType")
+          });
         }
         sections.push(asection);
       }
@@ -46,9 +65,9 @@ export class OdmviewComponent implements OnInit {
     this.files = event.files;
     for (const file of event.files) {
       file.fileEntry.file(odmfile => {
-        let reader:FileReader = new FileReader();
+        let reader: FileReader = new FileReader();
         var comp = this;
-        reader.onloadend =  function(e)  {
+        reader.onloadend = function (e) {
           // console.log(this.result);
           comp.loadODM(this.result);
         };
@@ -57,11 +76,9 @@ export class OdmviewComponent implements OnInit {
     }
   }
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   update() {
     console.log(this.dispODM);
@@ -72,3 +89,4 @@ export class OdmviewComponent implements OnInit {
     this.pageStart = page;
   }
 }
+

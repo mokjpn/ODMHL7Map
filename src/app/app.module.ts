3a8f522fca965b4a03797d0db263c3a47386de7f
Masafumi Okada <mf@okadajp.org>
2018-03-15 19:15:01 +0900

    Import angular-tree-component for SS-MIX2 Tree View.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FileDropModule } from 'ngx-file-drop';
+import { TreeModule } from 'angular-tree-component';
 
 
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     FormsModule,
-    FileDropModule
+    FileDropModule,
+    TreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]

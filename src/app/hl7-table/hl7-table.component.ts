54cb89b37beb06e72ae44f70049e1600afdd3ddb
Masafumi Okada <mf@okadajp.org>
2018-05-17 18:38:36 +0900

    Now ngx-dnd works.

--- src/app/hl7-table/hl7-table.component.ts
+++ src/app/hl7-table/hl7-table.component.ts
@@ -7,9 +7,9 @@ import {
   UploadFile
 } from 'ngx-file-drop';
 
-declare var require: any
-var hl72json = require('qewd-hl72json');
-var jsonToTable = require('json-to-table');
+declare var require: any;
+const hl72json = require('qewd-hl72json');
+const jsonToTable = require('json-to-table');
 
 @Component({
   selector: 'app-hl7-table',
@@ -17,30 +17,41 @@ var jsonToTable = require('json-to-table');
   styleUrls: ['./hl7-table.component.css']
 })
 export class Hl7TableComponent implements OnInit {
-
-  public loadHL7(message: string): void {
-    // console.log(message.split('\r'));
-    let jsonHL7: string;
-    jsonHL7 = hl72json(message.split('\r'), '2.5');
-    console.log(jsonHL7);
-    var obx = jsonHL7["OBX"];
-    this.hl7table = jsonToTable(obx);
-    this.hl7header = this.hl7table[0];
-    this.hl7contents = this.hl7table.slice(1, this.hl7table.length);
-    console.log(this.hl7table);
-  }
   public files: UploadFile[] = [];
   public hl7table;
   public hl7header = [];
   public hl7contents;
   public cols;
 
+  public filterValues(data) {
+    for (const key in data) {
+      if(data.hasOwnProperty(key)) {
+        if (data[key].hasOwnProperty('value')) {
+          data[key] = data[key].value;
+        } else if (typeof data[key] === 'object') {
+          this.filterValues(data[key]);
+        }
+      }
+    }
+  }
+  public loadHL7(message: string): void {
+    // console.log(message.split('\r'));
+    let jsonHL7: Array<Object>;
+    jsonHL7 = hl72json(message.split('\r'), '2.5');
+    const obx = jsonHL7['OBX'];
+    this.filterValues(obx);
+    this.hl7table = jsonToTable(obx);
+    this.hl7header = this.hl7table[0];
+    this.hl7contents = this.hl7table.slice(1, this.hl7table.length);
+    // console.log(this.hl7table);
+  }
+
   public dropped(event: UploadEvent) {
     this.files = event.files;
     for (const file of event.files) {
       file.fileEntry.file(hl7file => {
-        let reader: FileReader = new FileReader();
-        var comp = this;
+        const reader: FileReader = new FileReader();
+        const comp = this;
         reader.onloadend = function (e) {
           console.log(this.result);
           comp.loadHL7(this.result);
@@ -52,8 +63,8 @@ export class Hl7TableComponent implements OnInit {
 
   constructor() {
     this.hl7table = [
-      ["", ""],
-      ["", ""]
+      ['', ''],
+      ['', '']
     ];
   }
 

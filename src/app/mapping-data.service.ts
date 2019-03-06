84d14b33a8ef36da83c8c5db41b2349b01cc2a8d
Masafumi Okada <mf@okadajp.org>
2019-03-06 21:39:12 +0900

    マッピングを格納するサービスはCopyとConditional Copyに特化して実装

--- src/app/mapping-data.service.ts
+++ src/app/mapping-data.service.ts
@@ -1,103 +1,57 @@
 import { Injectable } from '@angular/core';
 
+declare var require: any;
+const rdf = require('rdf');
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class MappingDataService {
+  private store;
 
-  constructor() { }
-
-}
-
-export class Predicate {
-  public static dict  = {
-    'pred-copy': 'Copy',
-    'pred-composition': 'Composition'
-  };
-  private valuekey: string;
-  public getPredicateString() {
-    return (Predicate.dict[this.valuekey]);
-  }
-  public getPredicateURI() {
-    return (Mapping.namespace + this.valuekey);
-  }
-  public setPredicate(key: string) {
-    if (key in Predicate.dict) {
-      this.valuekey = key;
-    } else {
-      throw new Error('Invalid value of predicate');
+  getHL7BlankNode(odmNode) {
+    console.log('getHL7BlankNode:' + odmNode);
+    for (const triple of this.store.toArray()) {
+      if (triple.subject.equals(odmNode) && triple.predicate.equals('http://www.umin.ac.jp/cdisc/mapping/2019/03/ssmix2#copyfrom')) {
+        return(triple.object);
+      }
     }
+    return(null);
   }
-}
 
-// SPARQL Query terminology can be used to discribe detailed logical condition.
-export class Condition {
-  public static dict = {
-    'cond-equal': 'Equal',
-    'cond-notequal': 'Not Equal',
-    'cond-lessthan': 'Less Than',
-    'cond-lessthanequal': 'Less Than or Equal',
-    'cond-morethan': 'More Than',
-    'cond-morethanequal': 'More Than or Equal',
-    'cond-sparql': 'Specify SPARQL'
-  };
-  private valuekey: string;
-  public getConditionString() {
-    return (Condition.dict[this.valuekey]);
-  }
-  public getConditionURI() {
-    return (Mapping.namespace + this.valuekey);
-  }
-  public setCondition(key: string) {
-    if (key in Condition.dict) {
-      this.valuekey = key;
-    } else {
-      throw new Error('Invalid value of Condition');
+  getConditionValue(odmNode) {
+    const hl7node = this.getHL7BlankNode(odmNode);
+    if (hl7node != null) {
+      for (const triple of this.store.toArray()) {
+        if (triple.subject.equals(hl7node) && triple.predicate.equals('http://www.umin.ac.jp/cdisc/mapping/2019/03/ssmix2#condition')) {
+          const conditionNode = triple.object;
+          for (const t2 of this.store.toArray() ) {
+            if (t2.subject.equals(conditionNode) && t2.predicate.equals('http://www.umin.ac.jp/cdisc/mapping/2019/03/ssmix2#value')) {
+              return(t2.object);
+            }
+          }
+        }
+      }
     }
+    return(null);
+  }
+
+  add(triple) {
+    this.store.add(triple);
+  }
+
+  constructor() {
+    this.store = rdf.environment.createGraph();
+  }
+
+  output() {
+    let serialized = '';
+    this.store.forEach((triple) => {
+      serialized = serialized + triple.toNT() + '\n';
+    });
+    console.log('Serialized Mapping: ' + serialized);
+    return(serialized);
   }
 }
 
-export class Timing {
-  public static dict = {
-    'timing-same': 'From Same Visit',
-    'timing-multiple': 'From Multple Visits'
-  };
-  private valuekey: string;
-  public getTimingString() {
-    return (Timing.dict[this.valuekey]);
-  }
-  public getTimingURI() {
-    return (Mapping.namespace + this.valuekey);
-  }
-  public setTiming(key: string) {
-    if (key in Timing.dict) {
-      this.valuekey = key;
-    } else {
-      throw new Error('Invalid value of Timing');
-    }
-  }
-}
-
-export class Caluculation {
-  public static dict = {
-    'calc-count': 'Count filled',
-    'calc-mean': 'Mean',
-    'calc-median': 'Median',
-    'calc-max': 'Maximum',
-    'calc-min': 'Minimum',
-    'calc-sum': 'Sum',
-    'calc-r': 'Specify Method by R language'
-  };
-}
-
-export class Mapping {
-  public static namespace = 'http://www.umin.ac.jp/ssmixcdisc/2019/Mapping#';
-  public subject: string;
-  public predicate: Predicate;
-  public object: string;
-  public id: string;
-  public uri() {
-    return(this.id);
-  }
-}

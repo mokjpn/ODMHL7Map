545ab8160d859e75da4c4945a57bc41195cebd66
Masafumi Okada <mf@okadajp.org>
2019-03-06 21:40:27 +0900

    RDF出力ダイアログは実際にマッピングサービスから値を取得するように

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MappingDataService } from '../mapping-data.service';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +9,16 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private mapping: MappingDataService ) { }
 
   ngOnInit() {
   }
   public viewMap(event: any) {
     console.log(event);
+    console.log(this.mapping.output());
     const dialogRef = this.dialog.open(MapViewDialog, {
       width: '750px',
-      data: { rdfdata: '<http://www.umin.ac.jp/example> is "Just an Example"'}
+      data: { rdfdata: this.mapping.output() }
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
@@ -25,7 +27,7 @@ export class HeaderComponent implements OnInit {
 }
 
 export class MappingData {
-  rdfdata: string
+  rdfdata: string;
 }
 
 @Component({
@@ -37,7 +39,8 @@ export class MappingData {
 export class MapViewDialog {
     constructor(
       public dialogRef: MatDialogRef <MapViewDialog>,
-      @Inject(MAT_DIALOG_DATA) public data: MappingData) { }
+      @Inject(MAT_DIALOG_DATA) public data: MappingData) {
+      }
 
     onNoClick(): void {
       this.dialogRef.close();
